360f58f7b5b1991bec022f3acdbe60edc5417f46
fenykep <abel.fenykep@gmail.com>
2019-03-12 11:01:07 +0100

    batch adding

--- admin.php
+++ admin.php
@@ -31,10 +31,10 @@
 			<td><input class="form-control" type="text" name="ar" placeholder="Ár"></td>
 			<td><input class="form-control" type="text" name="szin" placeholder="Szín"></td>
 			<!--<td><input class="form-control" type="text" name="meret" placeholder="Méret"></td>-->
-			<td><input type="checkbox" name="meret" value="S">S</td>
-			<td><input type="checkbox" name="meret" value="M">M</td>
-			<td><input type="checkbox" name="meret" value="L">L</td>
-			<td><input type="checkbox" name="meret" value="XL">XL</td>
+			<td><input type="checkbox" name="meret[]" multiple="yes" value="S">S</td>
+			<td><input type="checkbox" name="meret[]" multiple="yes" value="M">M</td>
+			<td><input type="checkbox" name="meret[]" multiple="yes" value="L">L</td>
+			<td><input type="checkbox" name="meret[]" multiple="yes" value="XL">XL</td>
 			<td><input type="submit" value="Send"></td>
 		</tr>
 	</table>
--- sender.php
+++ sender.php
@@ -10,21 +10,6 @@ $password = "1kurvaberci";
 $dbname = "2033976_abel";
 */
 
-
-
-
-$termeknev = "kalap";
-$szin = "piros";
-$meret = "XL";
-$ar = 32000;
-
-//------NEMTOM MIRE VAN!!-------------------
-
-//$limit = intval($_GET['limit']); //casting to int type!
-//$query = "SELECT * FROM table LIMIT $limit";
-
-//------NEMTOM MIRE VAN!!-------------------
-
 // Create connection (with the sql "server?")
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
@@ -36,22 +21,18 @@ if (!$conn) {
 }
 else{echo("connected yeey!");}
 
-//itt küldöm el az sql-nek a form változóit, csak még szarul
-//------------------HUGE MESS!!!-----------------------------
+//itt küldöm el az sql-nek a form változóit
+echo count($_POST['meret']) ;
+print_r($_POST['meret']); 
+for ($i = 0; $i < count($_POST['meret']); $i++) {
+	$nagysag = $_POST['meret'][$i];
+    $sql = "INSERT INTO termekek (`id`, `termeknev`, `tag`, `ar`, `szin`, `meret`) VALUES (NULL, '$_POST[termeknev]', '$_POST[tag]', '$_POST[ar]', '$_POST[szin]', '$nagysag');";
+    echo "<br>".$sql."<br>";
+    if(mysqli_query($conn, $sql)===TRUE){echo("done!". $i);};
 
-$sql = "INSERT INTO termekek (`id`, `termeknev`, `tag`, `ar`, `szin`, `meret`) VALUES (NULL, '$_POST[termeknev]', '$_POST[tag]', '$_POST[ar]', '$_POST[szin]', '$_POST[meret]');";
+}
 
-$sql2 = "SELECT * FROM termekek;";
-
-echo $sql;
-if(mysqli_query($conn, $sql)===TRUE){echo("done!");};
-//mysqli_query($conn, $sql);
-//$rownum = mysqli_num_rows($lolo);
-//echo($rownum);
 mysqli_close($conn);
-//mysqli_close();
-//echo "Connected successfully";
-
 ?>
 
 <div>Back to <a href="admin.php">reality</a></div>
