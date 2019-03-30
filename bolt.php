5f481855278f08a38ac7484489df5b9db8901be5
fenykep <abel.fenykep@gmail.com>
2019-03-30 18:28:46 +0100

    most minden fasza

--- bolt.php
+++ bolt.php
@@ -10,19 +10,19 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </head>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</head>
 <body>
 
 <div class="container">
 
 <div class="nav-scroller py-1 mb-2">
-    <nav class="nav d-flex justify-content-center">
-      <a class="mx-auto p-3" href="index.php">Index</a>
-      <a class="mx-auto p-3 active" href="bolt.php">Bolt</a>
-      <a class="mx-auto p-3" href="kapcsolat.php">Kapcsolat</a>
-      <a class="mx-auto p-3" href="admin.php">Admin</a>
+  <nav class="nav d-flex justify-content-center">
+    <a class="mx-auto p-3" href="index.php">Index</a>
+    <a class="mx-auto p-3 active" href="bolt.php">Bolt</a>
+    <a class="mx-auto p-3" href="kapcsolat.php">Kapcsolat</a>
+    <a class="mx-auto p-3" href="admin.php">Admin</a>
   </nav>    
 </div>
 
@@ -44,7 +44,7 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 
 // Check connection
 if (!$conn) {die("Connection failed: " . mysqli_connect_error());}
-else{/*echo("<h1>Mi kéne ha vóna?</h1>"."<br>");*/}
+else{echo("");}
 
 $sql = "SELECT * FROM termekek LIMIT 15;";
 $result = $conn->query($sql);
@@ -115,8 +115,8 @@ if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
       echo '
           <div class="col-md-4">
-            <div class="card mb-4 shadow-sm border" style="width: 18rem;">
-              <img src="/www/img/cheese_white.jpg" class="card-img-top" alt="...">
+            <div class="card mb-4 shadow-sm border" style="width: 16rem;">
+              <img src="/www/img/'.$row["kep"].'" class="card-img-top" alt="...">
                 <div class="card-body">
                   <h5 class="card-title">' . $row["termeknev"]. '</h5>
                   <p class="card-text">Ide kerül majd a kártya szövege</p>
